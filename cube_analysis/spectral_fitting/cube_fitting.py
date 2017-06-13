f84f3ade2e8164da1a3de2132610e68d92eccc32
e-koch <koch.eric.w@gmail.com>
2017-06-13 15:48:16 -0600

    Function for fitting spectral models with a cube

---
+++ cube_analysis/spectral_fitting/cube_fitting.py
@@ -0,0 +1,58 @@
+
+'''
+Simplified per-spectrum fitting of a cube. Does not have many features
+(on purpose).
+
+See http://pyspeckit.readthedocs.io/en/latest/cubes.html or
+https://github.com/vlas-sokolov/multicube.
+
+'''
+
+import numpy as np
+from itertools import izip
+
+from ..progressbar import _map_context
+
+
+def cube_fitter(cube, fitting_func, args=(), kwargs={},
+                spatial_mask=None, verbose=False, num_cores=1):
+    '''
+    '''
+
+    if spatial_mask is not None:
+        if not cube[0].shape == spatial_mask.shape:
+            raise ValueError("spatial_mask must have the same shape as the "
+                             "cube's spatial dimensions.")
+    else:
+        spatial_mask = cube.mask.include().sum(0) > 0
+
+    posns = np.where(spatial_mask)
+
+    gener = ((fitting_func, args, kwargs, cube[:, y, x])
+             for y, x in izip(*posns))
+
+    with _map_context(num_cores, verbose=verbose) as map:
+        out_params = map(_fitter, gener)
+
+        npars = len(out_params[0][0]) if hasattr(out_params[0], "size") else 1
+
+        param_cube = np.empty((npars, ) + spatial_mask.shape)
+        error_cube = np.empty((npars, ) + spatial_mask.shape)
+
+        for out, y, x in izip(out_params, *posns):
+            param_cube[:, y, x] = out[0]
+            error_cube[:, y, x] = out[1]
+
+    empty_posns = np.where(~spatial_mask)
+    for i in range(npars):
+        param_cube[i][empty_posns] = np.NaN
+        error_cube[i][empty_posns] = np.NaN
+
+    return param_cube, error_cube
+
+
+def _fitter(inp_args):
+
+    fitting_func, args, kwargs, spec = inp_args
+
+    return fitting_func(spec, *args, **kwargs)
